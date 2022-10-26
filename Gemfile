fd802f197d9ea94e7c030a611e7603f9650fa61d
hukm <maximgook@gmail.com>
2022-10-26 16:54:53 +0200

    test aws sdk v3 instead of deprecated v1

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@ source 'https://rubygems.org'
 
 ruby '2.7.3'
 
-gem 'puma', '>= 3.12.3'
+gem 'puma', '4.3.6'
 gem 'rack', '>= 2.0.8'
-gem 'aws-sdk', '~> 1.4'
+gem 'aws-sdk-s3', '~> 1'
 gem 'heroku-log-parser', git: 'https://github.com/rwdaigle/heroku-log-parser.git'
 gem 'nokogiri', '>= 1.10.8'
 
--- writer/s3.rb
+++ writer/s3.rb
@@ -1,20 +1,56 @@
-require 'aws'
+require 'aws-sdk-s3'
 require 'logger'
 require_relative './base.rb'
 
 class Writer < WriterBase
 
-  S3_BUCKET_OBJECTS = AWS::S3.new({
-    access_key_id: ENV.fetch('S3_KEY'),
-    secret_access_key: ENV.fetch('S3_SECRET'),
-  }).buckets[ENV.fetch('S3_BUCKET')].objects
+  def list_bucket_objects(s3_client, bucket_name, max_objects = 50)
+    if max_objects < 1 || max_objects > 1000
+      puts 'Maximum number of objects to request must be between 1 and 1,000.'
+      return
+    end
+
+    objects = s3_client.list_objects_v2(
+      bucket: bucket_name,
+      max_keys: max_objects
+    ).contents
+
+    if objects.count.zero?
+      puts "No objects in bucket '#{bucket_name}'."
+      return
+    else
+      if objects.count == max_objects
+        puts "First #{objects.count} objects in bucket '#{bucket_name}':"
+      else
+        puts "Objects in bucket '#{bucket_name}':"
+      end
+      objects.each do |object|
+        puts object.key
+      end
+    end
+  rescue StandardError => e
+    puts "Error accessing bucket '#{bucket_name}' " \
+    "or listing its objects: #{e.message}"
+  end
+
+  s3_client = Aws::S3::Client.new({
+                                    access_key_id: ENV.fetch('S3_KEY'),
+                                    secret_access_key: ENV.fetch('S3_SECRET'),
+                                    region: ENV.fetch('AWS_REGION')
+                                  })
+
+  # S3_BUCKET_OBJECTS = AWS::S3.new({
+  #   access_key_id: ENV.fetch('S3_KEY'),
+  #   secret_access_key: ENV.fetch('S3_SECRET'),
+  # }).buckets[ENV.fetch('S3_BUCKET')].objects
 
   def stream_to(filepath)
     @logger.info "begin #{filepath}"
-    S3_BUCKET_OBJECTS[filepath].write(
-      @io,
-      estimated_content_length: 1 # low-ball estimate; so we can close buffer by returning nil
-    )
+    list_bucket_objects(s3_client, ENV.fetch('S3_BUCKET'))
+    # S3_BUCKET_OBJECTS[filepath].write(
+    #   @io,
+    #   estimated_content_length: 1 # low-ball estimate; so we can close buffer by returning nil
+    # )
     @logger.info "end #{filepath}"
   end
 
